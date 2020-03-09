5c1a041d94e9e8b84e5dc5f4ae6e6781395084ba
Ryzomych <rizoma@mail.riri>
2020-03-09 12:02:38 +0300

    changes in modal order window -  feature

--- php/order.php
+++ php/order.php
@@ -10,10 +10,10 @@ if(empty($_POST['name'])      ||
      return false;
    }
 
-$name = strip_tags(htmlspecialchars($_POST['name2']));
-$email_address = strip_tags(htmlspecialchars($_POST['email2']));
-$phone = strip_tags(htmlspecialchars($_POST['phone2']));
-$message = strip_tags(htmlspecialchars($_POST['message2']));
+$name = strip_tags(htmlspecialchars($_POST['name']));
+$email_address = strip_tags(htmlspecialchars($_POST['email']));
+$phone = strip_tags(htmlspecialchars($_POST['phone']));
+$message = strip_tags(htmlspecialchars($_POST['message']));
 
 // Create the email and send the message
 $to = 'team@falanster.by'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
