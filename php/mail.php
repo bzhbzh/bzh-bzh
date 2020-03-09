2e0851d8a5bf31a46d1cf0b88c1168a6e1c4d4dd
Ryzomych <rizoma@mail.riri>
2020-03-09 09:51:06 +0300

    email fixes

--- php/mail.php
+++ php/mail.php
@@ -16,10 +16,17 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
 // Create the email and send the message
-$to = 'fannrm@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'team@falanster.by'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "BZH-BZH Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+
+
+$headers = "From: falanster.by@gmail.com\r\n";
+$headers.= "MIME-Version: 1.0\r\n";
+$headers.= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+$headers.= "X-Priority: 1\r\n";
+
+//$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
 return true;
